a82338e737d445bed65e346c315a9ef7011f042e
Julia Justino <stephany.justino@sptech.school>
2023-10-28 23:15:51 -0300

    Add files via upload

--- python/pooPy/crud.py
+++ python/pooPy/crud.py
@@ -1,4 +1,7 @@
 import conexao
+import alerta
+import jira
+
 
 
 conexao = conexao.ConexaoBancoDeDados(
@@ -23,12 +26,29 @@ conexao.conexao.commit() """
 conexao.conexao.commit()"""
 
 # Lista os registros da tabela
-registros = cursor.execute(f"SELECT nome FROM tb_funcionario")
-for registro in registros:      
-    print(registros)
+registros = cursor.execute("SELECT uso_cpu FROM tb_registro")
+resultado = cursor.fetchall()
+for x in resultado:
+    print(x)
 
-pessoas = ["João", "Maria", "Pedro"]
-for pessoa in pessoas:
-    print(pessoa)
+resultadoInteiro = int(resultado)
+
+if resultado>75:
+    DadosSlack = alerta.DadosSlack(
+    mensagem=f"TESTE: CPU está fora das métricas {resultado}",
+    conexao = "https://hooks.slack.com/services/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"
+    
+)
+
+DadosSlack.enviar_mensagem()
+
+DadosJira = jira.DadosJira(
+    url = "https://stock-safe-solutions.atlassian.net/rest/api/3/issue",
+    responsavel="stephany.justino@sptech.school",
+    tokenConexao="ATATT3xFfGF0Ts4XoKjh8AwEkcWNOEIN8ertxXPiILZcVXoiZduTrNPF9DVcihsmue8q2m4FdrYm8U_Cg6iwpeotYG7xAY3nA90u4uMf_uz02yw-01Fl-wp3k9fu6XY384mZb9GHa4oGPRJZ9XIhsBxMDSaPLeAV0K0yjrxg6vc79w1Y_LurqyA=965A04E3"
+)
+
+DadosJira.abrirAlerta(f"TESTE: CPU está fora das métricas {resultado}", "STOCKSAFE", "Task")
+
 # Desconecta do banco de dados
 #conexao.desconectar()
